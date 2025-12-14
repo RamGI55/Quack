0067061b7efef4ed28f36ae46b027ba2f8c60c13
Hyunsoo Park (RamGi) <ram.ggi191@gmail.com>
2025-12-15 01:03:04 +0900

    Update: Wirte testing code

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
 
+#include "Component/InputComponent.h"
+#include "Component/SpritesComponent.h"
+#include "Component/TransformComponent.h"
+#include "Core/Coordinator.h"
+
+Coordinator gCoordinator;
 int main()
 {
     auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
     window.setFramerateLimit(144);
 
+    gCoordinator.Init();
+    gCoordinator.RegisterComponent<SpriteComponent>();
     while (window.isOpen())
     {
         while (const std::optional event = window.pollEvent())
