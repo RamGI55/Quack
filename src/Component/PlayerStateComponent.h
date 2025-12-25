68671381aee1ebeab07d45df587ce8a9c2d0543d
Hyunsoo Park <86937501+RamGI55@users.noreply.github.com>
2025-12-26 00:40:03 +0900

    fix: changed typo

--- src/Component/PlayerStateComponent.h
+++ src/Component/PlayerStateComponent.h
@@ -9,7 +9,7 @@
 struct PlayerStateComponent {
 
     int PlayerScore         = 0;
-    int RemainLives         = 0;
+    int LivesRemaining      = 0;
     std::string PlayerName  = "";
     float PlayTime          = 0.0f;
 
